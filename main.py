29a8818a6849db3bb5923a3c2064f2aa634eab46
Creighton Gorai <creighton-gorai@users.noreply.github.com>
2023-12-04 20:05:26 -1000

    Refactoring for bug where timer would always run instead of resetting

--- main.py
+++ main.py
@@ -4,50 +4,28 @@ import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = "Arial"
-REVEAL_CARD = 1.8
+
+current_card = {}
 
 
-# --------------------------- GUESSING TIMER ---------------------------- #
-def guessing_timer(count, word_dict):
-    timer = window.after(1000, guessing_timer, count - 1, word_dict)
-    if count < 0.0:
-        english_card(word_dict)
-        window.after_cancel(timer)
-
-
-# ---------------------------- NEXT CARD ------------------------------- #
-def new_card():
-    words = study_words()
-    french_card(words)
-    guessing_timer(REVEAL_CARD, words)
-
-
-def study_words():
-    try:
-        data = pandas.read_csv("data/words_to_learn.csv")
-    except FileNotFoundError:
-        data = pandas.read_csv("data/french_words.csv")
-        to_learn = data.to_dict(orient="records")
-        return random.choice(to_learn)
-    else:
-        to_learn = data.to_dict(orient="records")
-        return random.choice(to_learn)
-
-
-def french_card(french_word):
+def french_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
     canvas.create_image(400, 263, image=card_front)
     french_language_text = canvas.create_text(400, 150, text="Language", font=(FONT_NAME, 40, "italic"))
     french_word_text = canvas.create_text(400, 263, text="word", font=(FONT_NAME, 60, "bold"))
     canvas.itemconfig(french_language_text, text="French")
-    canvas.itemconfig(french_word_text, text=french_word["French"])
+    canvas.itemconfig(french_word_text, text=current_card["French"])
+    flip_timer = window.after(3000, func=english_card)
 
 
-def english_card(english_word):
+def english_card():
     canvas.create_image(400, 263, image=card_back)
     english_language_text = canvas.create_text(400, 150, text="Language", font=(FONT_NAME, 40, "italic"))
     english_word_text = canvas.create_text(400, 263, text="word", font=(FONT_NAME, 60, "bold"))
     canvas.itemconfig(english_language_text, text="English")
-    canvas.itemconfig(english_word_text, text=english_word["English"])
+    canvas.itemconfig(english_word_text, text=current_card["English"])
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -56,6 +34,8 @@ def english_card(english_word):
 window = Tk()
 window.title("Flash Cards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, func=english_card)
 
 # Create inside the window
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
@@ -68,16 +48,23 @@ language_text = canvas.create_text(400, 150, text="Language",  fill="black", fon
 word_text = canvas.create_text(400, 263, text="word",  fill="black", font=(FONT_NAME, 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
+# Could make word global and access in each function
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    data = pandas.read_csv("data/french_words.csv")
+    to_learn = data.to_dict(orient="records")
+
 # Create and show right button
 right_button = PhotoImage(file="images/right.png")
-known_button = Button(image=right_button, highlightthickness=0, command=new_card)
+known_button = Button(image=right_button, highlightthickness=0, command=french_card)
 known_button.grid(column=1, row=1)
 
 # Create and show wrong button
 wrong_button = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=wrong_button, highlightthickness=0, command=new_card)
+unknown_button = Button(image=wrong_button, highlightthickness=0, command=french_card)
 unknown_button.grid(column=0, row=1)
 
-new_card()
+french_card()
 window.mainloop()
 
